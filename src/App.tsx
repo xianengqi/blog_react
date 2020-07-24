85e9a73755f3d391f74cd8698134a6ecbb7f30ea
xianengqi <xianengqi@gmail.com>
2020-07-25 03:59:32 +0800

    feat: :wrench:使redux-saga流程跑通

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,20 @@ import { setRetryTip } from './redux/saga/actions/common';
 import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
-
   const dispatch = useDispatch();
-  const { retryTip } = useSelector((state: Iredux.IState) =>  state.common);
+  const { retryTip } = useSelector((state: Iredux.IState) => state.common);
   const handleTestReduxClick = () => {
     dispatch({
       type: 'TRIGGER',
-      payload: []
-    })
-  }
-  console.log('这是啥 =>',  retryTip, );
+      payload: [],
+    });
+  };
+  console.log('这是啥 =>', retryTip);
   return (
     <div className="App">
-      <Button onClick={handleTestReduxClick} type="primary">Hello world</Button>
+      <Button onClick={handleTestReduxClick} type="primary">
+        Hello world
+      </Button>
     </div>
   );
 }
