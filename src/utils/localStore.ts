3f0f92c90e5def3649efc40a3ec8d4cb1e5c7687
xianengqi <xianengqi@gmail.com>
2020-07-24 23:51:10 +0800

    feat: :beers:

---
+++ src/utils/localStore.ts
@@ -0,0 +1,67 @@
+export const store = window.localStorage;
+
+class localStore {
+  /**
+   * 设置数据: 如果value 是 object 会调用 JSON.stringify 自动转换为字符串
+   */
+  public static set(key: string, value: any) {
+    if (!store) {
+      return;
+    }
+    // 备份一份
+    let v = value;
+    try {
+      if (typeof value === 'object') {
+        v = JSON.stringify(v);
+      }
+      store.setItem(key, v);
+    } catch (error) {
+      // do...
+    }
+  }
+
+  /**
+   * 直接获取 --- 原始数据
+   */
+  public static get (key: string) {
+    if (!store) {
+      return;
+    }
+    return store.getItem(key);
+  }
+
+  /**
+   * 获取的同时转换为JSON
+   */
+  public static getJson (key: string) {
+    if (!store) {
+      return;
+    }
+    const data = store.getItem(key);
+    if (data) {
+      try {
+        return JSON.parse(data);
+      } catch (error) {
+        // do...
+      }
+    }
+    return null;
+  }
+
+  /**
+   * 删除本地存储
+   */
+  public static remove(key: string) {
+    if (!store) {
+      return;
+    }
+
+    try {
+      store.removeItem(key);
+    } catch (error) {
+      // do...
+    }
+  }
+}
+
+export default localStore;
